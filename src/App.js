d55197cc62d928ba43453171f8f8b6f70f4425db
Naixes <615411375@qq.com>
2020-06-05 11:33:02 +0800

    PriceList组件

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import PriceList from './components/PriceList'
+
+const items = [{
+  "id": 1,
+  "title": "去云南旅游",
+  "price": 200,
+  "date": "2019-1-23",
+  "category": {
+    "id": 1,
+    "name": "旅游",
+    "type": "outcome",
+    "iconName": "md-plane"
+  }
+}, {
+  "id": 2,
+  "title": "去云南旅游",
+  "price": 400,
+  "date": "2019-1-22",
+  "category": {
+    "id": 1,
+    "name": "旅游",
+    "type": "outcome",
+    "iconName": "ios-plane"
+  }
+}]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <PriceList
+        items = {items}
+        onModifyItem = {item => console.log(item)}
+        onDeleteItem = {item => console.log(item)}
+      ></PriceList>
     </div>
   );
 }
---
+++ src/components/PriceList.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Ionicon from 'react-ionicons'
+import {Colors} from '../util'
+
+const PriceList = ({items, onModifyItem, onDeleteItem}) => {
+    return (
+        <ul className="list-group">
+            {items.map(item => (
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                    <span className="col-1">
+                        <Ionicon
+                            // 圆
+                            className="rounded-circle"
+                            fontSize="30px"
+                            style={{backgroundColor: Colors.blue, padding: '5px'}}
+                            color={'#fff'}
+                            icon={item.category.iconName}
+                        ></Ionicon>
+                    </span>
+                    <span className="col-4">{item.title}</span>
+                    <span className="col-3 font-weight-bold">{item.category.type === 'outcome' ? '-' : '+'}{item.price}元</span>
+                    <span className="col-2">{item.date}</span>
+                    <a 
+                        href
+                        className="col-1" 
+                        role="button" 
+                        onClick={(e) => { e.preventDefault(); onModifyItem(item.id)}}
+                    >
+                        <Ionicon
+                            className="rounded-circle"
+                            fontSize="30px"
+                            style={{backgroundColor: Colors.blue, padding: '5px'}}
+                            color={'#fff'}
+                            icon='ios-create-outline'
+                        ></Ionicon>
+                    </a>
+                    <a 
+                        href
+                        className="col-1" 
+                        role="button" 
+                        onClick={(e) => { e.preventDefault(); onDeleteItem(item.id)}}
+                    >
+                        <Ionicon
+                            className="rounded-circle"
+                            fontSize="30px"
+                            style={{backgroundColor: Colors.blue, padding: '5px'}}
+                            color={'#fff'}
+                            icon='ios-close'
+                        ></Ionicon>
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+// 属性类型检查
+PriceList.propTypes = {
+    items: PropTypes.array.isRequired,
+    onModifyItem: PropTypes.func.isRequired,
+    onDeleteItem: PropTypes.func.isRequired
+}
+
+export default PriceList
